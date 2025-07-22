0f3ed7e95158a514f2e43d865de1aed716b7415a
AndrewCouncil <andrew.p.council@gmail.com>
2025-07-22 16:25:43 -0400

    build hugo based on my fork with change to chroma

--- flake.nix
+++ flake.nix
@@ -17,6 +17,19 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        myHugo = pkgs.hugo.overrideAttrs (old: {
+          version = "0.149.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "AndrewCouncil";
+            repo = "hugo";
+            rev = "8a8c94759021b8daaf1a2bb9fd2ab5d1ead0f5c9";
+            hash = "sha256-Kkt/4dC2gRaInQcZyhdpl2Q5kxhIrqj5PWlQ5+rlPr4=";
+          };
+          vendorHash = "sha256-iZwYP99elM64ljUjbj6i2YHTUHBc3B6NVfRXMyr4jSo=";
+          doCheck = false;
+        });
+
         treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
       in
       {
@@ -25,8 +38,8 @@
             (aspellWithDicts (ps: with ps; [ en ]))
             # keep-sorted start
             go
-            hugo
             mask
+            myHugo
             nushell
             zellij
             # keep-sorted end
