1f83ec35e97b5c372c164217dc6896a2f1676e0e
AndrewCouncil <andrew.p.council@gmail.com>
2025-07-22 01:13:29 -0400

    add formatter config stuff from new project template
    yamlfmt config

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
+      treefmt-nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -15,11 +17,21 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = [ pkgs.hugo ];
+          buildInputs = with pkgs; [
+            (aspellWithDicts (ps: with ps; [ en ]))
+            # keep-sorted start
+            hugo
+            nushell
+            # keep-sorted end
+          ];
         };
+
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
       }
     );
 }
---
+++ treefmt.nix
@@ -0,0 +1,30 @@
+{
+  ...
+}:
+{
+  projectRootFile = "flake.nix";
+
+  programs = {
+    nixfmt.enable = true;
+    jsonfmt.enable = true;
+    shellcheck.enable = true;
+    gofmt.enable = true;
+    ruff.enable = true;
+    yamlfmt.enable = true;
+    toml-sort.enable = true;
+    dos2unix.enable = true;
+    keep-sorted.enable = true;
+    # buggy as of right now
+    # nufmt.enable = true;
+  };
+
+  settings = {
+    # files to exlude from all formatting
+    excludes = [
+      "themes/*"
+    ];
+    formatter = {
+      # formatter-specific settings
+    };
+  };
+}
